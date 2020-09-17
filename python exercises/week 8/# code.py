8da3686d9c391251ede4e259f35622d42dcc8469
will-twosticks <63495970+will-twosticks@users.noreply.github.com>
2020-09-17 14:31:52 +1000

    added files

---
+++ python exercises/week 8/# code.py
@@ -0,0 +1,27 @@
+FirstName = []
+LastName = []
+Age = []
+Year = 2020
+User = 0
+
+# users personal data
+while True:
+    FirstInput = input("what is your first name? ")
+    if FirstInput  == "":
+        break
+
+    FirstName.append(FirstInput)
+
+    SecondInput = input("what is your last name? ")
+    LastName.append(SecondInput)
+
+    ThirdInput = int(input("what is your age? "))
+    Age.append(ThirdInput)
+    User += 1
+
+# output user name and password of users
+for x in range(User):
+    Username = (FirstName[x][:1]) + (LastName[x]) + ("@Huawow.io")
+    Password = (FirstName[x]) + (LastName[x][:1]) + str(Year - Age[x] - 9)
+    print(Username + "|" + Password)
+    print("-------------------------")
